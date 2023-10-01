4d2d2c761848fab23d802e388964e8342a887224
nachoaladro <ignacioaladro385@gmail.com>
2023-10-01 18:16:01 -0300

    Agregue objetos, arrays y funciones de orden superior

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,98 @@
-console.log("Hola equipo docende de Coder para la comision 47060 de JS. En este trabajo van a encontrar el comienzo de un proyecto que va a buscar ser una herramienta para las personas que, como yo, operan los mercados de valores todos los dias y utilizamos hermmaientas que suelen estar en diferentes lugares. La idea principal es poder amalgamarlas en una pantalla facil de ver y navegar. Veremos como sale xD")
+console.log("Estan viendo un simulador de conversion de monedas, por el momento el tipo de cambio va a ser fijo. A futuro la idea seria que la plataforma tome ese dato de otro lado para que se actualice todo el tiempo.");
 
-console.log("Aqui comienza la 1° Pre Entrega del Proyecto Final")
-
-// Estan viendo un simulador de conversion de monedas, por el momento es el usuario quien va a ingresar los tipos de cambio. A futuro la idea seria que la plataforma tome ese dato de otro lado
-
-function multiplicador(capital, tipoDeCambio) {
-    let resultado = capital * tipoDeCambio;
-    return resultado;
+function conversor(capital, tipoDeCambio) {
+    let resultado = capital / tipoDeCambio;
+    return resultado.toFixed(2);
 }
+
+function conversorCripto(capital, tipoDeCambio) {
+    let resultado = capital / tipoDeCambio;
+    return resultado.toFixed(8);
+}
+
+const TIPO_DE_CAMBIO = {
+    eur: 847,
+    usd: 805,
+    cny: 110,
+    btc: 9501396
+};
+
+let ultimaConversion = {};
+const HISTORIAL = [];
 
 let respuestaUsuario = prompt("Hola, bienvenid@ al conversor de divisas\n¿Quieres convertir una divisa?");
 
 while (respuestaUsuario.toUpperCase() == "SI") {
-    const DIVISA_UNO = prompt("Elije la divisa que quieres cambiar\nEUR -- USD -- ARS -- CNY -- BTC")
-    const CAPITAL = parseFloat(prompt("Ingrese la cantidad de nominales que quiere cambiar"))
-    const DIVISA_DOS = prompt("Ingrese la divisa POR la que quiere cambiar\nEUR -- USD -- ARS -- CNY -- BTC");
-    const TIPO_DE_CAMBIO = parseFloat(prompt("Ingrese el tipo de cambio"));
+    const CAPITAL = parseFloat(prompt("Ingrese la cantidad de nominales en pesos argentinos que quiere cambiar"))
+    const DIVISA_CAMBIO = prompt("Ingrese la divisa por la que quiere cambiar\nEUR -- USD -- CNY -- BTC");
 
-    if (!isNaN(CAPITAL) && !isNaN(TIPO_DE_CAMBIO)) {
-        switch (DIVISA_UNO.trim().toUpperCase()) {
+    if (!isNaN(CAPITAL)) {
+        switch (DIVISA_CAMBIO.trim().toUpperCase()) {
             case "EUR":
-                alert("El monto nominal de " + CAPITAL + " euros representa " + multiplicador(CAPITAL, TIPO_DE_CAMBIO) + " " + DIVISA_DOS); 
+                alert("El monto nominal de " + CAPITAL + " pesos argentinos representa " + conversor(CAPITAL, TIPO_DE_CAMBIO.eur) + " " + DIVISA_CAMBIO);
+                ultimaConversion = {
+                    tuDinero: CAPITAL,
+                    monedaQueElegiste: DIVISA_CAMBIO,
+                    resultado: conversor(CAPITAL, TIPO_DE_CAMBIO.eur)
+                }
+                HISTORIAL.unshift(ultimaConversion);
                 break;
             case "USD":
-                alert("El monto nominal de " + CAPITAL + " dolares estadounidenses representa " + multiplicador(CAPITAL, TIPO_DE_CAMBIO) + " " + DIVISA_DOS); 
-                break;
-            case "ARS":
-                alert("El monto nominal de " + CAPITAL + " pesos argentinos representa " + multiplicador(CAPITAL, TIPO_DE_CAMBIO) + " " + DIVISA_DOS); 
+                alert("El monto nominal de " + CAPITAL + " pesos argentinos representa " + conversor(CAPITAL, TIPO_DE_CAMBIO.usd) + " " + DIVISA_CAMBIO);
+                ultimaConversion = {
+                    tuDinero: CAPITAL,
+                    monedaQueElegiste: DIVISA_CAMBIO,
+                    resultado: conversor(CAPITAL, TIPO_DE_CAMBIO.usd)
+                }
+                HISTORIAL.unshift(ultimaConversion);
                 break;
             case "CNY":
-                alert("El monto nominal de " + CAPITAL + " yuanes chinos representa" + multiplicador(CAPITAL, TIPO_DE_CAMBIO) + " " + DIVISA_DOS); 
+                alert("El monto nominal de " + CAPITAL + " pesos argentinos representa " + conversor(CAPITAL, TIPO_DE_CAMBIO.cny) + " " + DIVISA_CAMBIO);
+                ultimaConversion = {
+                    tuDinero: CAPITAL,
+                    monedaQueElegiste: DIVISA_CAMBIO,
+                    resultado: conversor(CAPITAL, TIPO_DE_CAMBIO.cny)
+                }
+                HISTORIAL.unshift(ultimaConversion);
                 break;
             case "BTC":
-                alert("El monto nominal de " + CAPITAL + " bitcoins representa" + multiplicador(CAPITAL, TIPO_DE_CAMBIO) + " " + DIVISA_DOS); 
+                alert("El monto nominal de " + CAPITAL + " pesos argentinos representa " + conversorCripto(CAPITAL, TIPO_DE_CAMBIO.btc) + " " + DIVISA_CAMBIO);
+                ultimaConversion = {
+                    tuDinero: CAPITAL,
+                    monedaQueElegiste: DIVISA_CAMBIO,
+                    resultado: conversorCripto(CAPITAL, TIPO_DE_CAMBIO.btc)
+                }
+                HISTORIAL.unshift(ultimaConversion);
                 break;
             default:
-                alert("Por el momento las divisas con las que contamos son EUR, USD, ARS, CNY, BTC");
+                alert("Por el momento las divisas con las que contamos son EUR, USD, CNY, BTC");
                 break;
         }
     } else {
         alert("Por favor ingrese los nominales en numeros");
     }
+
+    console.log(HISTORIAL);
+
     respuestaUsuario = prompt("¿Quieres realizar una nueva conversion?");
+}
+
+let filtrarHistorial = prompt("¿Quiere ver el historial aplicando un flitro por divisa?") 
+
+while (filtrarHistorial.toUpperCase() == "SI") {
+    const DIVISA_FILTRO = prompt("¿Por cual divisa quiere filtrar el historial?\nEUR -- USD -- CNY -- BTC")
+    const HISTORIAL_FILTRADO = HISTORIAL.filter(
+        conversionHecha => {
+        return (conversionHecha.monedaQueElegiste.trim().toLowerCase().includes(DIVISA_FILTRO.trim().toLowerCase()));
+    });
+
+    if (HISTORIAL_FILTRADO.length !== 0) {
+        console.table(HISTORIAL_FILTRADO);
+    } else {
+        console.log("No se encunetran converiosnes realizadas con esa divisa.");
+    }
+
+    filtrarHistorial = prompt("¿Desea volver a intentar filtrar el historial?");
 }
 
 
